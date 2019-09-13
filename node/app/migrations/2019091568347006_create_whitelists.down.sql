2b81952d10dc8546a971873cd3d7006e1e9d6ab6
Tanagorn Suttijit <tanagorn@lightnet.io>
2019-09-13 11:45:21 +0700

    add migration files

---
+++ node/app/migrations/2019091568347006_create_whitelists.down.sql
@@ -0,0 +1,2 @@
+DROP "whitelists",
+DROP "roles";
---
+++ node/app/migrations/2019091568347006_create_whitelists.up.sql
@@ -0,0 +1,34 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE "public"."whitelists"
+(
+  "id"          uuid DEFAULT uuid_generate_v4()             PRIMARY KEY NOT NULL,
+  "address"     varchar(200)                                NOT NULL,
+  "role"        varchar(100)                                NOT NULL,
+  "created_at"  timestamptz                                 NOT NULL,
+  "updated_at"  timestamptz                                 NOT NULL,
+  "deleted_at"  timestamptz
+);
+
+CREATE TABLE "public"."roles"
+(
+  "id"          serial                                      PRIMARY KEY NOT NULL,
+  "name"        varchar(100)                                NOT NULL,
+  "role"        varchar(100)                                NOT NULL UNIQUE,
+  "created_at"  timestamptz                                 NOT NULL,
+  "updated_at"  timestamptz                                 NOT NULL,
+  "deleted_at"  timestamptz
+);
+
+CREATE UNIQUE INDEX whitelists_partial_unique_code1 ON "public"."whitelists" (address, role, deleted_at)
+WHERE deleted_at IS NOT NULL;
+
+CREATE UNIQUE INDEX whitelists_partial_unique_code2 ON "public"."whitelists" (address, role)
+WHERE deleted_at IS NULL;
+
+INSERT INTO public.roles
+ (name, role, created_at, updated_at) VALUES
+('Whitelist user', 'whitelist-user', current_timestamp, current_timestamp),
+('Set up token', 'setup-token', current_timestamp, current_timestamp),
+('Mint token', 'mint-token', current_timestamp, current_timestamp),
+('Feed price to Node', 'feed-price', current_timestamp, current_timestamp);
