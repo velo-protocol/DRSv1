c22569a0e62b8181c87b061ca20f513c14c8c4f0
Tanagorn Suttijit <tanagorn@lightnet.io>
2019-09-16 14:55:15 +0700

    change structure of database (already review with our team)

--- node/app/migrations/2019091568347006_create_whitelists.up.sql
+++ node/app/migrations/2019091568347006_create_whitelists.up.sql
@@ -3,8 +3,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE "public"."whitelists"
 (
   "id"          uuid DEFAULT uuid_generate_v4()             PRIMARY KEY NOT NULL,
-  "stellar_address"     varchar(200)                        NOT NULL,
-  "role"        varchar(100)                                NOT NULL,
+  "stellar_public_address"     varchar(200)                 NOT NULL,
+  "role_code"   varchar(100)                                NOT NULL,
   "created_at"  timestamptz                                 NOT NULL,
   "updated_at"  timestamptz                                 NOT NULL,
   "deleted_at"  timestamptz
@@ -14,20 +14,22 @@ CREATE TABLE "public"."roles"
 (
   "id"          serial                                      PRIMARY KEY NOT NULL,
   "name"        varchar(100)                                NOT NULL,
-  "role"        varchar(100)                                NOT NULL UNIQUE,
+  "code"        varchar(100)                                NOT NULL UNIQUE,
   "created_at"  timestamptz                                 NOT NULL,
   "updated_at"  timestamptz                                 NOT NULL,
   "deleted_at"  timestamptz
 );
 
-CREATE UNIQUE INDEX whitelists_partial_unique_code1 ON "public"."whitelists" (stellar_address, role, deleted_at)
+CREATE UNIQUE INDEX whitelists_partial_unique_code1 ON "public"."whitelists" (stellar_public_address, role_code, deleted_at)
 WHERE deleted_at IS NOT NULL;
 
-CREATE UNIQUE INDEX whitelists_partial_unique_code2 ON "public"."whitelists" (stellar_address, role)
+CREATE UNIQUE INDEX whitelists_partial_unique_code2 ON "public"."whitelists" (stellar_public_address, role_code)
 WHERE deleted_at IS NULL;
 
+ALTER TABLE public.whitelists ADD CONSTRAINT whitelists_fk FOREIGN KEY ("role_code") REFERENCES public.roles("code");
+
 INSERT INTO public.roles
- (name, role, created_at, updated_at) VALUES
+ (name, code, created_at, updated_at) VALUES
 ('KYC checker', 'REGULATOR', current_timestamp, current_timestamp),
 ('Trusted partner', 'TRUSTED_PARTNER', current_timestamp, current_timestamp),
 ('Price feeder', 'PRICE_FEEDER', current_timestamp, current_timestamp);
