ee995405d0fe0a53d5d4f8d54e865cf518334d0f
Tanagorn Suttijit <tanagorn@lightnet.io>
2019-09-16 13:29:22 +0700

    change data in role table

--- node/app/migrations/2019091568347006_create_whitelists.up.sql
+++ node/app/migrations/2019091568347006_create_whitelists.up.sql
@@ -28,5 +28,6 @@ WHERE deleted_at IS NULL;
 
 INSERT INTO public.roles
  (name, role, created_at, updated_at) VALUES
+('KYC checker', 'REGULATOR', current_timestamp, current_timestamp),
 ('Trusted partner', 'TRUSTED_PARTNER', current_timestamp, current_timestamp),
 ('Price feeder', 'PRICE_FEEDER', current_timestamp, current_timestamp),
