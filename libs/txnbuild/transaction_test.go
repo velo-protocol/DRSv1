4147e5b5d2e22d7b0c255e451d85e9531904a434
Jura Boonnom <jura@lightnet.io>
2019-09-23 17:14:25 +0700

    add test cases

--- libs/txnbuild/transaction_test.go
+++ libs/txnbuild/transaction_test.go
@@ -2,6 +2,7 @@ package vtxnbuild
 
 import (
 	"github.com/stellar/go/txnbuild"
+	"github.com/stellar/go/xdr"
 	"github.com/stretchr/testify/assert"
 
 	"gitlab.com/velo-labs/cen/libs/xdr"
@@ -32,7 +33,97 @@ func TestVeloTx_Build(t *testing.T) {
 		// assert equality of tx in envelope and the velo tx
 		assert.Equal(t, veloTx.veloXdrTx.VeloOp, veloTx.veloXdrEnvelope.VeloTx.VeloOp)
 	})
-	// TODO: Add error test case
+
+	t.Run("success, tx has been assigned with envelope, reset it", func(t *testing.T) {
+		veloTx := VeloTx{
+			SourceAccount: &txnbuild.SimpleAccount{
+				AccountID: publicKey1,
+			},
+			VeloOp: &WhiteList{
+				Address: publicKey2,
+				Role:    string(vxdr.RoleTrustedPartner),
+			},
+		}
+		veloTx.veloXdrEnvelope = &vxdr.VeloTxEnvelope{}
+
+		err := veloTx.Build()
+		assert.NoError(t, err)
+
+		// assert data in xdr tx
+		assert.Equal(t, vxdr.OperationTypeWhiteList, veloTx.veloXdrTx.VeloOp.Body.Type)
+		assert.Equal(t, publicKey1, veloTx.veloXdrTx.SourceAccount.Address())
+		assert.Equal(t, publicKey2, veloTx.veloXdrTx.VeloOp.Body.WhiteListOp.Address.Address())
+		assert.Equal(t, vxdr.RoleTrustedPartner, veloTx.veloXdrTx.VeloOp.Body.WhiteListOp.Role)
+
+		// assert equality of tx in envelope and the velo tx
+		assert.Equal(t, veloTx.veloXdrTx.VeloOp, veloTx.veloXdrEnvelope.VeloTx.VeloOp)
+	})
+
+	t.Run("error, tx has already been signed, cannot be rebuilt", func(t *testing.T) {
+		veloTx := VeloTx{
+			SourceAccount: &txnbuild.SimpleAccount{
+				AccountID: publicKey1,
+			},
+			VeloOp: &WhiteList{
+				Address: publicKey2,
+				Role:    string(vxdr.RoleTrustedPartner),
+			},
+		}
+
+		veloTx.veloXdrEnvelope = &vxdr.VeloTxEnvelope{
+			Signatures: []xdr.DecoratedSignature{
+				{Signature: []byte("SOME_SIGNATURE")},
+			},
+		}
+
+		err := veloTx.Build()
+		assert.Error(t, err)
+	})
+
+	t.Run("error, invalid public key format, the key must start with G", func(t *testing.T) {
+		veloTx := VeloTx{
+			SourceAccount: &txnbuild.SimpleAccount{
+				AccountID: "BAD_PUBLIC_KEY",
+			},
+			VeloOp: &WhiteList{
+				Address: publicKey2,
+				Role:    string(vxdr.RoleTrustedPartner),
+			},
+		}
+
+		err := veloTx.Build()
+		assert.Error(t, err)
+	})
+
+	t.Run("error, invalid public key format", func(t *testing.T) {
+		veloTx := VeloTx{
+			SourceAccount: &txnbuild.SimpleAccount{
+				AccountID: "G_BAD_PUBLIC_KEY",
+			},
+			VeloOp: &WhiteList{
+				Address: publicKey2,
+				Role:    string(vxdr.RoleTrustedPartner),
+			},
+		}
+
+		err := veloTx.Build()
+		assert.Error(t, err)
+	})
+
+	t.Run("error, VeloOp validation fail, bad role", func(t *testing.T) {
+		veloTx := VeloTx{
+			SourceAccount: &txnbuild.SimpleAccount{
+				AccountID: publicKey1,
+			},
+			VeloOp: &WhiteList{
+				Address: publicKey2,
+				Role:    "BAD_ROLE",
+			},
+		}
+
+		err := veloTx.Build()
+		assert.Error(t, err)
+	})
 }
 
 func TestVeloTx_Sign(t *testing.T) {
@@ -68,7 +159,6 @@ func TestVeloTx_Sign(t *testing.T) {
 			string(veloTx.veloXdrEnvelope.Signatures[0].Signature) ==
 				string(veloTx.veloXdrEnvelope.Signatures[1].Signature))
 	})
-	// TODO: Add error test case
 }
 
 func TestVeloTx_BuildSignEncode(t *testing.T) {
@@ -119,5 +209,9 @@ func TestTransactionFromXDR(t *testing.T) {
 			string(veloTx.veloXdrEnvelope.Signatures[0].Signature) ==
 				string(veloTx.veloXdrEnvelope.Signatures[1].Signature))
 	})
-	// TODO: Add error test case
+
+	t.Run("error, unable to unmarshal velo xdr", func(t *testing.T) {
+		_, err := TransactionFromXDR("AAAAAAAABAD_XDR")
+		assert.Error(t, err)
+	})
 }
