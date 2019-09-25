efde2a3b00fa2f4a6eb594c73ff818681d558c50
Maris Kheawsaad <maris@lightnet.io>
2019-09-25 07:01:49 +0000

    Fixed success message of SetupCredit op

--- node/app/constants/reply_messages.go
+++ node/app/constants/reply_messages.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	ReplyWhiteListSuccess   = "Address %s is successfully added to whitelist with role %s"
-	ReplySetupCreditSuccess = "Setup credit successfully"
-	ReplyPriceUpdateSuccess = "Price updated successfully"
+	ReplyWhiteListSuccess   = "Address %s is successfully added to whitelist with role %s."
+	ReplySetupCreditSuccess = "Setup credit operation returned. Please sign the transaction and submit to Stellar for the operation to be completed. The operation will expire in the next 15 minutes."
+	ReplyPriceUpdateSuccess = "Price updated successfully."
 )
