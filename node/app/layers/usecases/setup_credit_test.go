c0459f01a6c59c1410c10730eeb04af791d75324
WasinTaw <dreamwstt@gmail.com>
2019-10-01 12:56:47 +0700

    Add setup credit unit test

--- node/app/layers/usecases/setup_credit_test.go
+++ node/app/layers/usecases/setup_credit_test.go
@@ -3,6 +3,7 @@ package usecases_test
 import (
 	"context"
 	"encoding/base64"
+	"fmt"
 	"github.com/pkg/errors"
 	"github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/txnbuild"
@@ -11,6 +12,7 @@ import (
 	vtxnbuild "gitlab.com/velo-labs/cen/libs/txnbuild"
 	"gitlab.com/velo-labs/cen/node/app/constants"
 	"gitlab.com/velo-labs/cen/node/app/entities"
+	nerrors "gitlab.com/velo-labs/cen/node/app/errors"
 	"testing"
 )
 
@@ -67,14 +69,31 @@ func TestUseCase_SetupCredit(t *testing.T) {
 
 		// get trusted partner meta
 		helper.mockStellarRepo.EXPECT().GetAccountData(trustedPartnerMetaAddress).
-			Return(map[string]string{"SGD_GAN6D232HXTF4OHL7J36SAJD3M22H26B2O4QFVRO32OEM523KTMB6Q72": "R0RXQUZZM1pRSlZEQ0tOVVVOTFZHNTVOVkZCRFpWVlBZRFNGWlIzRURQTEtJWkwzNDRKWkxUNlU="}, nil)
+			Return(map[string]string{"vSGD_GAN6D232HXTF4OHL7J36SAJD3M22H26B2O4QFVRO32OEM523KTMB6Q72": "R0RXQUZZM1pRSlZEQ0tOVVVOTFZHNTVOVkZCRFpWVlBZRFNGWlIzRURQTEtJWkwzNDRKWkxUNlU="}, nil)
 
 		signedStellarTxXdr, err := helper.useCase.SetupCredit(context.Background(), veloTx)
 		assert.NoError(t, err)
 		assert.NotNil(t, signedStellarTxXdr)
 	})
 
+	t.Run("Error - velo op validation fail", func(t *testing.T) {
+		helper := initTest(t)
+		defer helper.mockController.Finish()
 
+		veloTx := &vtxnbuild.VeloTx{
+			SourceAccount: &txnbuild.SimpleAccount{
+				AccountID: publicKey1,
+			},
+			VeloOp: &vtxnbuild.SetupCredit{
+				PeggedCurrency: "",
+			},
+		}
+
+		signedStellarTxXdr, err := helper.useCase.SetupCredit(context.Background(), veloTx)
+		assert.Error(t, err)
+		assert.Nil(t, signedStellarTxXdr)
+		assert.IsType(t, nerrors.ErrInvalidArgument{}, err)
+	})
 
 	t.Run("Error - VeloTx missing signer", func(t *testing.T) {
 		helper := initTest(t)
@@ -89,6 +108,7 @@ func TestUseCase_SetupCredit(t *testing.T) {
 		assert.Nil(t, signedStellarTxXdr)
 		assert.NotNil(t, err)
 		assert.Contains(t, err.Error(), constants.ErrSignatureNotFound)
+		assert.IsType(t, nerrors.ErrUnAuthenticated{}, err)
 	})
 
 	t.Run("Error - VeloTx wrong signer", func(t *testing.T) {
@@ -104,15 +124,16 @@ func TestUseCase_SetupCredit(t *testing.T) {
 		assert.Nil(t, signedStellarTxXdr)
 		assert.NotNil(t, err)
 		assert.Contains(t, err.Error(), constants.ErrSignatureNotMatchSourceAccount)
+		assert.IsType(t, nerrors.ErrUnAuthenticated{}, err)
 	})
 
-	t.Run("Error - can't query on whitelist on stellar", func(t *testing.T) {
+	t.Run("Error - can't get whitelist", func(t *testing.T) {
 		helper := initTest(t)
 		defer helper.mockController.Finish()
 
 		helper.mockStellarRepo.EXPECT().
 			GetAccount(publicKey1).
-			Return(nil, errors.New("stellar error"))
+			Return(nil, errors.New("stellar return error"))
 
 		veloTx := getMockVeloTx()
 		_ = veloTx.Build()
@@ -121,74 +142,218 @@ func TestUseCase_SetupCredit(t *testing.T) {
 		signedStellarTxXdr, err := helper.useCase.SetupCredit(context.Background(), veloTx)
 		assert.Nil(t, signedStellarTxXdr)
 		assert.Contains(t, err.Error(), "fail to get tx sender account")
+		assert.IsType(t, nerrors.ErrNotFound{}, err)
 	})
 
-	//t.Run("Error - this user has no permission", func(t *testing.T) {
-	//	helper := initTest(t)
-	//	defer mockCtrl.Finish()
-	//
-	//	testHelper.MockWhiteListRepo.EXPECT().
-	//		FindOneWhitelist(entities.WhiteListFilter{
-	//			StellarPublicKey: &publicKey1,
-	//			RoleCode:         pointer.ToString(string(vxdr.RoleTrustedPartner)),
-	//		}).
-	//		Return(nil, nil)
-	//
-	//	veloTx := getMockVeloTx()
-	//	_ = veloTx.Build()
-	//	_ = veloTx.Sign(kp1)
-	//
-	//	_, err := useCase.SetupCredit(context.Background(), veloTx)
-	//	assert.Contains(t, err.Error(), fmt.Sprintf(constants.ErrFormatSignerNotHavePermission, constants.VeloOpSetupCredit))
-	//	assert.IsType(t, nerrors.ErrPermissionDenied{}, err)
-	//})
-	//
-	//t.Run("Error - fail to load trusted partner account", func(t *testing.T) {
-	//	helper := initTest(t)
-	//	defer mockCtrl.Finish()
-	//
-	//	testHelper.MockWhiteListRepo.EXPECT().
-	//		FindOneWhitelist(entities.WhiteListFilter{
-	//			StellarPublicKey: &publicKey1,
-	//			RoleCode:         pointer.ToString(string(vxdr.RoleTrustedPartner)),
-	//		}).
-	//		Return(&entities.WhiteList{StellarPublicKey: publicKey1}, nil)
-	//
-	//	helper.mockStellarRepo.EXPECT().
-	//		GetAccount(publicKey1).
-	//		Return(nil, errors.New("some error has occurred"))
-	//
-	//	veloTx := getMockVeloTx()
-	//	_ = veloTx.Build()
-	//	_ = veloTx.Sign(kp1)
-	//
-	//	_, err := useCase.SetupCredit(context.Background(), veloTx)
-	//	assert.IsType(t, nerrors.ErrNotFound{}, err)
-	//})
-	//
-	//t.Run("Error - fail to build tx, bad tp account format", func(t *testing.T) {
-	//	helper := initTest(t)
-	//	defer mockCtrl.Finish()
-	//
-	//	testHelper.MockWhiteListRepo.EXPECT().
-	//		FindOneWhitelist(entities.WhiteListFilter{
-	//			StellarPublicKey: &publicKey1,
-	//			RoleCode:         pointer.ToString(string(vxdr.RoleTrustedPartner)),
-	//		}).
-	//		Return(&entities.WhiteList{StellarPublicKey: publicKey1}, nil)
-	//
-	//	helper.mockStellarRepo.EXPECT().
-	//		GetAccount(publicKey1).
-	//		Return(&horizon.Account{
-	//			AccountID: "GBAD_ACCOUNT",
-	//		}, nil)
-	//
-	//	veloTx := getMockVeloTx()
-	//	_ = veloTx.Build()
-	//	_ = veloTx.Sign(kp1)
-	//
-	//	_, err := useCase.SetupCredit(context.Background(), veloTx)
-	//	assert.IsType(t, nerrors.ErrInternal{}, err)
-	//})
+	t.Run("Error - can't get DRS account data", func(t *testing.T) {
+		helper := initTest(t)
+		defer helper.mockController.Finish()
+
+		veloTx := getMockVeloTx()
+		_ = veloTx.Build()
+		_ = veloTx.Sign(kp1)
+
+		// get tx sender account
+		helper.mockStellarRepo.EXPECT().
+			GetAccount(publicKey1).
+			Return(&horizon.Account{
+				AccountID: publicKey1,
+				Sequence:  "1",
+			}, nil)
+
+		// get drs account data
+		helper.mockStellarRepo.EXPECT().GetDrsAccountData().
+			Return(nil, errors.New("stellar return error"))
+
+		signedStellarTxXdr, err := helper.useCase.SetupCredit(context.Background(), veloTx)
+		assert.Error(t, err)
+		assert.Nil(t, signedStellarTxXdr)
+		assert.Contains(t, err.Error(), "fail to get data of drs account")
+		assert.IsType(t, nerrors.ErrInternal{}, err)
+	})
+
+	t.Run("Error - can't get trusted partner role", func(t *testing.T) {
+		helper := initTest(t)
+		defer helper.mockController.Finish()
+
+		veloTx := getMockVeloTx()
+		_ = veloTx.Build()
+		_ = veloTx.Sign(kp1)
+
+		// get tx sender account
+		helper.mockStellarRepo.EXPECT().
+			GetAccount(publicKey1).
+			Return(&horizon.Account{
+				AccountID: publicKey1,
+				Sequence:  "1",
+			}, nil)
+
+		// get drs account data
+		helper.mockStellarRepo.EXPECT().GetDrsAccountData().
+			Return(
+				&entities.DrsAccountData{
+					TrustedPartnerListAddress: trustedPartnerListAddress,
+				},
+				nil)
+
+		// validate trusted partner role
+		helper.mockStellarRepo.EXPECT().GetAccountData(trustedPartnerListAddress).
+			Return(nil, errors.New("stellar return error"))
+
+		signedStellarTxXdr, err := helper.useCase.SetupCredit(context.Background(), veloTx)
+		assert.Error(t, err)
+		assert.Nil(t, signedStellarTxXdr)
+		assert.Contains(t, err.Error(), "fail to get data of trusted partner list account")
+		assert.IsType(t, nerrors.ErrInternal{}, err)
+	})
+
+	t.Run("Error - don't have trusted partner permission denied", func(t *testing.T) {
+		helper := initTest(t)
+		defer helper.mockController.Finish()
+
+		veloTx := getMockVeloTx()
+		_ = veloTx.Build()
+		_ = veloTx.Sign(kp1)
+
+		// get tx sender account
+		helper.mockStellarRepo.EXPECT().
+			GetAccount(publicKey1).
+			Return(&horizon.Account{
+				AccountID: publicKey1,
+				Sequence:  "1",
+			}, nil)
+
+		// get drs account data
+		helper.mockStellarRepo.EXPECT().GetDrsAccountData().
+			Return(
+				&entities.DrsAccountData{
+					TrustedPartnerListAddress: trustedPartnerListAddress,
+				},
+				nil)
+
+		// validate trusted partner role
+		helper.mockStellarRepo.EXPECT().GetAccountData(trustedPartnerListAddress).
+			Return(map[string]string{}, nil)
+
+		signedStellarTxXdr, err := helper.useCase.SetupCredit(context.Background(), veloTx)
+		assert.Error(t, err)
+		assert.Nil(t, signedStellarTxXdr)
+		assert.Equal(t, err.Error(), fmt.Sprintf(constants.ErrFormatSignerNotHavePermission, constants.VeloOpSetupCredit))
+		assert.IsType(t, nerrors.ErrPermissionDenied{}, err)
+	})
+
+	t.Run("Error - can't decode trusted partner data", func(t *testing.T) {
+		helper := initTest(t)
+		defer helper.mockController.Finish()
+
+		veloTx := getMockVeloTx()
+		_ = veloTx.Build()
+		_ = veloTx.Sign(kp1)
+
+		// get tx sender account
+		helper.mockStellarRepo.EXPECT().
+			GetAccount(publicKey1).
+			Return(&horizon.Account{
+				AccountID: publicKey1,
+				Sequence:  "1",
+			}, nil)
+
+		// get drs account data
+		helper.mockStellarRepo.EXPECT().GetDrsAccountData().
+			Return(
+				&entities.DrsAccountData{
+					TrustedPartnerListAddress: trustedPartnerListAddress,
+				},
+				nil)
+
+		// validate trusted partner role
+		helper.mockStellarRepo.EXPECT().GetAccountData(trustedPartnerListAddress).
+			Return(map[string]string{publicKey1: "BAD_ENCODED_VALUE"}, nil)
+
+		signedStellarTxXdr, err := helper.useCase.SetupCredit(context.Background(), veloTx)
+		assert.Error(t, err)
+		assert.Nil(t, signedStellarTxXdr)
+		assert.Contains(t, err.Error(), "fail to decode data")
+		assert.IsType(t, nerrors.ErrInternal{}, err)
+	})
+
+	t.Run("Error - can't get trusted partner meta", func(t *testing.T) {
+		helper := initTest(t)
+		defer helper.mockController.Finish()
+
+		veloTx := getMockVeloTx()
+		_ = veloTx.Build()
+		_ = veloTx.Sign(kp1)
+
+		// get tx sender account
+		helper.mockStellarRepo.EXPECT().
+			GetAccount(publicKey1).
+			Return(&horizon.Account{
+				AccountID: publicKey1,
+				Sequence:  "1",
+			}, nil)
+
+		// get drs account data
+		helper.mockStellarRepo.EXPECT().GetDrsAccountData().
+			Return(
+				&entities.DrsAccountData{
+					TrustedPartnerListAddress: trustedPartnerListAddress,
+				},
+				nil)
+
+		// validate trusted partner role
+		helper.mockStellarRepo.EXPECT().GetAccountData(trustedPartnerListAddress).
+			Return(map[string]string{publicKey1: trustedPartnerMetaEncoded}, nil)
+
+		// get trusted partner meta
+		helper.mockStellarRepo.EXPECT().GetAccountData(trustedPartnerMetaAddress).
+			Return(nil, errors.New("stellar return error"))
+
+		signedStellarTxXdr, err := helper.useCase.SetupCredit(context.Background(), veloTx)
+		assert.Error(t, err)
+		assert.Nil(t, signedStellarTxXdr)
+		assert.IsType(t, nerrors.ErrInternal{}, err)
+	})
+
+	t.Run("Error - the issuing and distribution account for asset code to specified already", func(t *testing.T) {
+		helper := initTest(t)
+		defer helper.mockController.Finish()
+
+		veloTx := getMockVeloTx()
+		_ = veloTx.Build()
+		_ = veloTx.Sign(kp1)
+
+		// get tx sender account
+		helper.mockStellarRepo.EXPECT().
+			GetAccount(publicKey1).
+			Return(&horizon.Account{
+				AccountID: publicKey1,
+				Sequence:  "1",
+			}, nil)
+
+		// get drs account data
+		helper.mockStellarRepo.EXPECT().GetDrsAccountData().
+			Return(
+				&entities.DrsAccountData{
+					TrustedPartnerListAddress: trustedPartnerListAddress,
+				},
+				nil)
+
+		// validate trusted partner role
+		helper.mockStellarRepo.EXPECT().GetAccountData(trustedPartnerListAddress).
+			Return(map[string]string{publicKey1: trustedPartnerMetaEncoded}, nil)
+
+		// get trusted partner meta
+		helper.mockStellarRepo.EXPECT().GetAccountData(trustedPartnerMetaAddress).
+			Return(map[string]string{
+				"vSGD_GAN6D232HXTF4OHL7J36SAJD3M22H26B2O4QFVRO32OEM523KTMB6Q72": "R0RXQUZZM1pRSlZEQ0tOVVVOTFZHNTVOVkZCRFpWVlBZRFNGWlIzRURQTEtJWkwzNDRKWkxUNlU=",
+				"vTHB_GAHLHUVDHRJ3U3CUOYQRW2TVNRIC6QC6R2MWVCMKYSVYESO5CQMA6PYM": "R0NTWExLS0tFRzdDWE9WVEVTRVI2SDRYNkk0V1lIWkFCVkpNRkxFUU42MlVCTFNMVlhPUEFUSFY="}, nil)
+
+		signedStellarTxXdr, err := helper.useCase.SetupCredit(context.Background(), veloTx)
+		assert.Error(t, err)
+		assert.Nil(t, signedStellarTxXdr)
+		assert.IsType(t, nerrors.ErrInternal{}, err)
+		assert.Equal(t, "the issuing and distribution account for asset code to specified already", err.Error())
+	})
 
 }
