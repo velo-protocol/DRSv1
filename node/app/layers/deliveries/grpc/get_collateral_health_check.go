536de49b602c8c1388f97884023f7a0a7d947b4b
Wasin Tawatkittaya <dream@r0ix.com>
2019-10-25 05:24:33 +0000

    Feature/velo 213 create collateral health check handler

---
+++ node/app/layers/deliveries/grpc/get_collateral_health_check.go
@@ -0,0 +1,21 @@
+package grpc
+
+import (
+	"context"
+	spec "gitlab.com/velo-labs/cen/grpc"
+)
+
+func (handler handler) GetCollateralHealthCheck(ctx context.Context, empty *spec.Empty) (*spec.GetCollateralHealthCheckReply, error) {
+
+	getCollateralHealthCheck, err := handler.UseCase.GetCollateralHealthCheck(ctx)
+	if err != nil {
+		return nil, err.GRPCError()
+	}
+
+	return &spec.GetCollateralHealthCheckReply{
+		AssetCode:      getCollateralHealthCheck.AssetCode,
+		AssetIssuer:    getCollateralHealthCheck.AssetIssuer,
+		RequiredAmount: getCollateralHealthCheck.RequiredAmount.StringFixed(7),
+		PoolAmount:     getCollateralHealthCheck.PoolAmount.StringFixed(7),
+	}, nil
+}
---
+++ node/app/layers/deliveries/grpc/get_collateral_health_check_test.go
@@ -0,0 +1,53 @@
+package grpc_test
+
+import (
+	"context"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/velo-labs/cen/grpc"
+	"gitlab.com/velo-labs/cen/node/app/entities"
+	nerrors "gitlab.com/velo-labs/cen/node/app/errors"
+	"testing"
+)
+
+func TestHandler_GetCollateralHealthCheck(t *testing.T) {
+
+	var (
+		assetCode      = "vTHB"
+		assetIssuer    = "GCQDOOHRLBZW2A6COOMMWI5RAKGEZPBXSGZ6L6WA7M7GK3ZMHODDRAS3"
+		requiredAmount = decimal.NewFromFloat(150.2230124)
+		poolAmount     = decimal.NewFromFloat(250.0092210)
+	)
+
+	t.Run("success", func(t *testing.T) {
+		helper := initTest(t)
+		helper.mockController.Finish()
+
+		helper.mockUseCase.EXPECT().GetCollateralHealthCheck(context.Background()).Return(&entities.GetCollateralHealthCheckOutput{
+			AssetCode:      assetCode,
+			AssetIssuer:    assetIssuer,
+			RequiredAmount: requiredAmount,
+			PoolAmount:     poolAmount,
+		}, nil)
+
+		getCollateralHealthCheckOutput, err := helper.handler.GetCollateralHealthCheck(context.Background(), &grpc.Empty{})
+		assert.NoError(t, err)
+		assert.NotNil(t, getCollateralHealthCheckOutput)
+		assert.Equal(t, assetCode, getCollateralHealthCheckOutput.AssetCode)
+		assert.Equal(t, assetIssuer, getCollateralHealthCheckOutput.AssetIssuer)
+		assert.Equal(t, requiredAmount.StringFixed(7), getCollateralHealthCheckOutput.RequiredAmount)
+		assert.Equal(t, poolAmount.StringFixed(7), getCollateralHealthCheckOutput.PoolAmount)
+
+	})
+
+	t.Run("error, use case return error", func(t *testing.T) {
+		helper := initTest(t)
+		helper.mockController.Finish()
+
+		helper.mockUseCase.EXPECT().GetCollateralHealthCheck(context.Background()).Return(nil, nerrors.ErrInternal{Message: "some error has occurred"})
+
+		getCollateralHealthCheckOutput, err := helper.handler.GetCollateralHealthCheck(context.Background(), &grpc.Empty{})
+		assert.Error(t, err)
+		assert.Nil(t, getCollateralHealthCheckOutput)
+	})
+}
