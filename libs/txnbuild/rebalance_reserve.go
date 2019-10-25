25094b9bc41fa0fea8e8b383a69b2489c37f0569
Wasin Tawatkittaya <dream@r0ix.com>
2019-10-25 11:04:35 +0000

    Feature/velo 222 add rebalance reserve operation

---
+++ libs/txnbuild/rebalance_reserve.go
@@ -0,0 +1,34 @@
+package vtxnbuild
+
+import (
+	"github.com/pkg/errors"
+	"gitlab.com/velo-labs/cen/libs/xdr"
+)
+
+type RebalanceReserve struct{}
+
+func (rebalanceReserve *RebalanceReserve) BuildXDR() (vxdr.VeloOp, error) {
+
+	// xdr op
+	vXdrOp := vxdr.RebalanceReserveOp{}
+
+	body, err := vxdr.NewOperationBody(vxdr.OperationTypeRebalanceReserve, vXdrOp)
+	if err != nil {
+		return vxdr.VeloOp{}, errors.Wrap(err, "failed to build XDR operation body")
+	}
+
+	return vxdr.VeloOp{Body: body}, nil
+}
+
+func (rebalanceReserve *RebalanceReserve) FromXDR(vXdrOp vxdr.VeloOp) error {
+	redeemOp := vXdrOp.Body.RebalanceReserveOp
+	if redeemOp == nil {
+		return errors.New("error parsing rebalanceReserve operation from xdr")
+	}
+
+	return nil
+}
+
+func (rebalanceReserve *RebalanceReserve) Validate() error {
+	return nil
+}
