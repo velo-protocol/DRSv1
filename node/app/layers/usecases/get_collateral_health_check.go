2ca0190fabb91bc161067308914ff8d4fbe6d0e6
athiwatp <athiwat.p5@gmail.com>
2019-11-27 14:33:33 +0700

    add wait group to rebalance and get collateral health check for improve time to call horizon testnet, change horizon testnet from stellar to velo horizon testnet server improve time to call get assets

--- node/app/layers/usecases/get_collateral_health_check.go
+++ node/app/layers/usecases/get_collateral_health_check.go
@@ -10,6 +10,7 @@ import (
 	"github.com/velo-protocol/DRSv1/node/app/environments"
 	"github.com/velo-protocol/DRSv1/node/app/errors"
 	"strings"
+	"sync"
 )
 
 func (useCase *useCase) GetCollateralHealthCheck(ctx context.Context) (*entities.GetCollateralHealthCheckOutput, nerrors.NodeError) {
@@ -56,62 +57,80 @@ func (useCase *useCase) GetCollateralHealthCheck(ctx context.Context) (*entities
 
 	var drsCollateralRequiredAmount = decimal.Zero
 
-	// calculate drs collateral required amount amount
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(len(tpListData))
+	var waitGroupErr nerrors.NodeError
+
+	// calculate drs collateral required amount
 	for _, tpMetaAddress := range tpListData {
-		tpMetaData, err := useCase.StellarRepo.GetAccountDecodedData(tpMetaAddress)
-		if err != nil {
-			return nil, nerrors.ErrPrecondition{
-				Message: errors.Wrap(err, constants.ErrGetTrustedPartnerMetaAccountDetail).Error(),
-			}
-		}
 
-		// calculate drs collateral required amount amount per tp
-		var collateralPerTp = decimal.Zero
-		for stableCredit := range tpMetaData {
-			assetDetail := strings.Split(stableCredit, "_")
-			if len(assetDetail) != 2 {
-				return nil, nerrors.ErrPrecondition{Message: constants.ErrVerifyAssetCode}
-			}
-
-			issuerAccount, err := useCase.SubUseCase.GetIssuerAccount(ctx, &entities.GetIssuerAccountInput{IssuerAddress: assetDetail[1]})
+		go func(tpMetaAddress string) {
+			defer func() {
+				if r := recover(); r != nil {
+					waitGroupErr = r.(nerrors.NodeError)
+				}
+				waitGroup.Done()
+			}()
+			tpMetaData, err := useCase.StellarRepo.GetAccountDecodedData(tpMetaAddress)
 			if err != nil {
-				return nil, nerrors.ErrPrecondition{Message: errors.Wrap(err, constants.ErrGetIssuerAccount).Error()}
+				panic(nerrors.ErrPrecondition{
+					Message: errors.Wrap(err, constants.ErrGetTrustedPartnerMetaAccountDetail).Error(),
+				})
 			}
 
-			assetPage, err := useCase.StellarRepo.GetAsset(entities.GetAssetInput{
-				AssetCode:   assetDetail[0],
-				AssetIssuer: assetDetail[1],
-			})
-			if err != nil {
-				return nil, nerrors.ErrPrecondition{Message: errors.Wrapf(err, constants.ErrGetAsset, assetDetail[0]).Error()}
-			}
+			// calculate drs collateral required amount per tp
+			var collateralPerTp = decimal.Zero
+			for stableCredit := range tpMetaData {
+				assetDetail := strings.Split(stableCredit, "_")
+				if len(assetDetail) != 2 {
+					panic(nerrors.ErrPrecondition{Message: constants.ErrVerifyAssetCode})
+				}
 
-			if len(assetPage.Embedded.Records) < 1 {
-				continue
-			}
+				issuerAccount, err := useCase.SubUseCase.GetIssuerAccount(ctx, &entities.GetIssuerAccountInput{IssuerAddress: assetDetail[1]})
+				if err != nil {
+					panic(nerrors.ErrPrecondition{Message: errors.Wrap(err, constants.ErrGetIssuerAccount).Error()})
+				}
 
-			stableAmount, err := decimal.NewFromString(assetPage.Embedded.Records[0].Amount)
-			if err != nil {
-				return nil, nerrors.ErrPrecondition{Message: errors.Wrapf(err, "invalid stable amount format").Error()}
-			}
+				assetPage, err := useCase.StellarRepo.GetAsset(entities.GetAssetInput{
+					AssetCode:   assetDetail[0],
+					AssetIssuer: assetDetail[1],
+				})
 
-			var collateralPerCredit decimal.Decimal
-			switch vxdr.Currency(issuerAccount.PeggedCurrency) {
-			case vxdr.CurrencyTHB:
-				collateralPerCredit = stableAmount.Mul(issuerAccount.PeggedValue).Div(medianPriceThb)
-			case vxdr.CurrencySGD:
-				collateralPerCredit = stableAmount.Mul(issuerAccount.PeggedValue).Div(medianPriceSgd)
-			case vxdr.CurrencyUSD:
-				collateralPerCredit = stableAmount.Mul(issuerAccount.PeggedValue).Div(medianPriceUsd)
-			default:
-				return nil, nerrors.ErrInternal{Message: constants.ErrPeggedCurrencyIsNotSupport}
+				if err != nil {
+					panic(nerrors.ErrPrecondition{Message: errors.Wrapf(err, constants.ErrGetAsset, assetDetail[0]).Error()})
+				}
 
+				if len(assetPage.Embedded.Records) < 1 {
+					continue
+				}
+
+				stableAmount, err := decimal.NewFromString(assetPage.Embedded.Records[0].Amount)
+				if err != nil {
+					panic(nerrors.ErrPrecondition{Message: errors.Wrapf(err, "invalid stable amount format").Error()})
+				}
+
+				var collateralPerCredit decimal.Decimal
+				switch vxdr.Currency(issuerAccount.PeggedCurrency) {
+				case vxdr.CurrencyTHB:
+					collateralPerCredit = stableAmount.Mul(issuerAccount.PeggedValue).Div(medianPriceThb)
+				case vxdr.CurrencySGD:
+					collateralPerCredit = stableAmount.Mul(issuerAccount.PeggedValue).Div(medianPriceSgd)
+				case vxdr.CurrencyUSD:
+					collateralPerCredit = stableAmount.Mul(issuerAccount.PeggedValue).Div(medianPriceUsd)
+				default:
+					panic(nerrors.ErrInternal{Message: constants.ErrPeggedCurrencyIsNotSupport})
+				}
+				// sum total drs collateral required amount of tp
+				collateralPerTp = collateralPerTp.Add(collateralPerCredit)
 			}
-			// sum total drs collateral required amount of tp
-			collateralPerTp = collateralPerTp.Add(collateralPerCredit)
-		}
-		// sum total drs collateral required amount amount
-		drsCollateralRequiredAmount = drsCollateralRequiredAmount.Add(collateralPerTp)
+			// sum total drs collateral required amount
+			drsCollateralRequiredAmount = drsCollateralRequiredAmount.Add(collateralPerTp)
+		}(tpMetaAddress)
+	}
+
+	waitGroup.Wait()
+	if waitGroupErr != nil {
+		return nil, waitGroupErr
 	}
 
 	// get drs collateral amount
